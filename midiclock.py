1388968e9f71aa0c1273aeb9e59d1796561a8aed
Lennart Regebro <regebro@gmail.com>
2023-11-26 17:17:40 +0100

    Try to get more accurate clock

--- midiclock.py
+++ midiclock.py
@@ -1,9 +1,16 @@
 import mido
 import time
+import sys
 
 mido.set_backend("mido.backends.pygame")
 msg = mido.Message("clock")
-out = mido.open_output('MIDIFACE 16x16 MIDI 1', autoreset=True)
+
+# My test ports
+if sys.platform == "linux":
+    port = 'Midi Through Port-0'
+else:
+    port = 'DIN 1'
+out = mido.open_output(port, autoreset=True)
 
 start = time.time_ns()
 count = 0
@@ -14,12 +21,22 @@ while True:
     # 120bpm = 120*24 ticks per minute = 48 ticks per second =
     # 1E9/48 ns/tick 
     n = int(count*5E8//24)
-#    print(count)
     tick_ns = start + n
     sc = 0
+    pmax = 0
+    pmin = 1E20
+    pt = 0
     while True:
-        time.sleep(0.000001)  # Sleep one microsecond
-        if time.time_ns() >= tick_ns:
+        time.sleep(0)  # Sleep as little as possible
+        pold = pt
+        pt = time.time_ns()
+        if pold != 0:
+            pdiff = pt - pold
+            pmin = min(pdiff, pmin)
+            pmax = max(pdiff, pmax)
+        p = pt - tick_ns
+        if p > 0:
+            print(sc, p, pmin, pmax)
             break
         sc += 1
     
